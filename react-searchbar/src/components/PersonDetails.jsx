433042ce47075ca6f27608081e59dc60e51dee7a
Nick Tse <nicholashtse@gmail.com>
2023-03-25 01:28:09 -0400

    update PersonDetails display and spacing

--- react-searchbar/src/components/PersonDetails.jsx
+++ react-searchbar/src/components/PersonDetails.jsx
@@ -1,42 +1,43 @@
 import React from "react";
-import "../css/PersonDetails.css"
+import "../css/PersonDetails.css";
 
 export const PersonDetails = ({ clickedUser }) => {
   // console.log("clickedUser", clickedUser);
   return (
     <div className="person-details-container">
-    <ul>
-      {Object.keys(clickedUser).map((field) => {
-        if (typeof clickedUser[field] === "object") {
-          return (
-            <>
-              <li key={field}>
-                <b>{field}:</b>
-                <ul>
-                  {Object.keys(clickedUser[field]).map((innerField) => {
-                    return (
-                      typeof clickedUser[field][innerField] !== "object" && (
-                        <li key={innerField}>
-                          {innerField}: {clickedUser[field][innerField]}
-                        </li>
-                      )
-                    );
-                  })}
-                </ul>
-              </li>
-            </>
-          );
-        } else {
-          return (
-            field !== "id" && (
-              <li key={field}>
-                <b>{field}:</b> {clickedUser[field]}
-              </li>
-            )
-          );
-        }
-      })}
-    </ul>
+      <h1>User Details</h1>
+      <ul>
+        {Object.keys(clickedUser).map((field) => {
+          if (typeof clickedUser[field] === "object") {
+            return (
+              <>
+                <li key={field}>
+                  <b>{field}:</b>
+                  <ul>
+                    {Object.keys(clickedUser[field]).map((innerField) => {
+                      return (
+                        typeof clickedUser[field][innerField] !== "object" && (
+                          <li key={innerField}>
+                            {innerField}: {clickedUser[field][innerField]}
+                          </li>
+                        )
+                      );
+                    })}
+                  </ul>
+                </li>
+              </>
+            );
+          } else {
+            return (
+              field !== "id" && (
+                <li key={field}>
+                  <b>{field}:</b> {clickedUser[field]}
+                </li>
+              )
+            );
+          }
+        })}
+      </ul>
     </div>
   );
 };
